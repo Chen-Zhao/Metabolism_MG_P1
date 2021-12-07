1aafb38d19cf3703484efc9d6cb8bee27120a4af
cz <zhaochen.tcm@gmail.com>
2021-12-07 17:32:25 +0100

    add conti test

--- src/99_figure_and_table.R
+++ src/99_figure_and_table.R
@@ -31,3 +31,43 @@ r2_nagelkerke_f <- function(model, l_base) {
   r2_nagelkerke
 }
 
+test_r2_conti_f <- function(d_r2_tmp,B=200){
+  d_r2_tmp <- d_r2_tmp[colSums(apply(d_r2_tmp,1,is.na))==0,]
+  Nt <- nrow(d_r2_tmp)
+  fit_tmp <- lm(y~x,d_r2_tmp)
+  cd <- cooks.distance(fit_tmp)
+  Ncd <- sum(cd>4)
+  if(Ncd>0){
+    d_r2_tmp <- d_r2_tmp[cd<=4,]
+  }
+  fit_tmp <- lm(y ~ x,d_r2_tmp)
+  fit_tmp_0 <- lm(y ~ 1,d_r2_tmp)
+  
+  shapiro_test_res <- shapiro.test(resid(fit_tmp))$p.value
+  cook_weisberg_res <- ncvTest(fit_tmp)$p
+  #adj_r2 <- summary(fit_tmp)$adj.r.squared
+  adj_r2 <- summary(fit_tmp)$r.squared
+  df <- attr(logLik(fit_tmp),"df")
+  df_0 <- attr(logLik(fit_tmp_0),"df")
+  lrtest_p <- pchisq(2*abs(logLik(fit_tmp)-logLik(fit_tmp_0)),df=df-df_0,lower.tail = F)
+  
+  adj_r2_bootstrap <- sapply(1:B,function(i){
+    fit_tmp <- lm(y ~ x+1,d_r2_tmp[sample(1:NROW(d_r2_tmp),NROW(d_r2_tmp),replace = TRUE),])
+    #summary(fit_tmp)$adj.r.squared
+    summary(fit_tmp)$r.squared
+  })
+  adj_r2_bootstrap_se = sqrt(sum((adj_r2_bootstrap-mean(adj_r2_bootstrap))^2)/(B-1))
+  
+  
+  c(shapiro_test_res,cook_weisberg_res,adj_r2,adj_r2_bootstrap_se,lrtest_p,Nt,Ncd)
+}
+
+
+## create dataset
+
+load("../RLS_META_MG/to_stepwise_conditional.RData")
+
+
+
+
+
